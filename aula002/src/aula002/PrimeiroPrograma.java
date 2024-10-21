29641c12d0150c3aa5ee5a9ae4fbadc05e1e4617
KauePiotto <kauepiotto@gmail.com>
2024-10-21 16:42:49 -0300

    curso-udemy

--- aula002/src/aula002/PrimeiroPrograma.java
+++ aula002/src/aula002/PrimeiroPrograma.java
@@ -2,6 +2,7 @@ package aula002;
 
 public class PrimeiroPrograma {
 	public static void main(String[] args) {
-		System.out.println("Hello Mundo!");
+		System.out.println("Primeiro Programa Parte #01");
+		System.out.println("Primeiro Programa Parte #02");
 	}
 }
