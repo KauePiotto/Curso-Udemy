2a61946fa29c5bb4df058a4bdbb0b73caa00223c
KauePiotto <kauepiotto@gmail.com>
2025-02-12 19:30:05 -0300

    Conversao de temperatura de ºF para ºC

--- VariavelEconstante/src/AulaVariavelEConstante/AreaCircuferencia.java
+++ VariavelEconstante/src/AulaVariavelEConstante/AreaCircuferencia.java
@@ -1,5 +1,29 @@
+package AulaVariavelEConstante;
 public class AreaCircuferencia {
     public static void main(String[] args) {
-        System.out.println("Funcionou");
-    }
+        /*
+         * Tipos de variaveis em java:
+         * 
+         * Int = numero inteiro
+         * Double = numero decimal
+         * 
+         * Final serve para nao ter como alterar o valor
+         * 
+         * Sempre usar nome de constante com letra maiuscula
+         */
+        double raio = 3.4;
+        final double PI = 3.14159;
+        double area;
+
+        area = PI * raio * raio;
+        
+        System.out.println(area);
+
+        raio = 10;
+
+        area = PI * raio * raio;
+
+        System.out.println("Área = " + area + " m2.");
+
+   }
 }
---
+++ VariavelEconstante/src/AulaVariavelEConstante/ConversaoDeTemperatura.java
@@ -0,0 +1,26 @@
+package AulaVariavelEConstante;
+
+import java.util.Scanner;
+
+public class ConversaoDeTemperatura {
+	public static void main(String[] args) {
+		/*Formula usada nesse Desafio (ºF - 32) * 5 / 9 = ºC]
+		*Colocar o 32 de uma constante de nome ajuste
+		* 5 e 9 de x e y
+		*/
+
+		Scanner scan = new Scanner(System.in);
+		double f = 0 ;
+		double c ;
+		final double ajuste = 32;
+		final int y = 5;
+		final double z = 9.0;
+
+		System.out.println("Informe a temperatura em Fahrenheit");
+		f = scan.nextDouble();
+
+		c = (f - ajuste) * y / z;
+
+		System.out.println("A temperatura em Celsius é " + c);
+	}
+}
