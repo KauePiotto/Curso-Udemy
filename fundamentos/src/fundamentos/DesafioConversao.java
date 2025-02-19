cc43adfb873882617d80b4069e4dfa665c2141b3
KauePiotto <kauepiotto@gmail.com>
2025-02-19 16:18:21 -0300

    Desafio da média dos três salários

--- fundamentos/src/fundamentos/DesafioConversao.java
+++ fundamentos/src/fundamentos/DesafioConversao.java
@@ -10,21 +10,21 @@ public class DesafioConversao {
 		String salario2;
 		String salario3;
 
+		//o Comando .replace() muda quando o usuario colcoa "," para "."
 		System.out.println("Digite o valor do seu primeiro mês de salario: ");
-		salario1 = scan.next();
+		salario1 = scan.next().replace(",", ".");
 		System.out.println("Digite o valor do seu segundo mês de trabalho:");
-		salario2 = scan.next();
+		salario2 = scan.next().replace(",", ".");
 		System.out.println("Digite o valor do seu terceiro mês de trabalho:");
-		salario3 = scan.next();
+		salario3 = scan.next().replace(",", ".");
 
 		double sal1 = Double.parseDouble(salario1);
 		double sal2 = Double.parseDouble(salario2);
 		double sal3 = Double.parseDouble(salario3);
 
-		double soma = sal1 + sal2 + sal3;
+		double soma = (sal1 + sal2 + sal3) / 3;
 
-		System.out.println("A soma dos três salários é R$" + soma);
-		System.out.println("A média dos três salários é R$" + soma);
+		System.out.println("A média dos salários é R$" + soma);
 
 		scan.close();
 	}
